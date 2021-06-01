fb66ee8aee20e592363a27bc388af444cde4c73c
jiangtianyi <jiangtianyi@TeamCity>
2021-06-01 18:42:26 +0800

    TeamCity change in '123' project: Versioned settings configuration updated

---
+++ .teamcity/settings.kts
@@ -0,0 +1,41 @@
+import jetbrains.buildServer.configs.kotlin.v2018_2.*
+
+/*
+The settings script is an entry point for defining a TeamCity
+project hierarchy. The script should contain a single call to the
+project() function with a Project instance or an init function as
+an argument.
+
+VcsRoots, BuildTypes, Templates, and subprojects can be
+registered inside the project using the vcsRoot(), buildType(),
+template(), and subProject() methods respectively.
+
+To debug settings scripts in command-line, run the
+
+    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
+
+command and attach your debugger to the port 8000.
+
+To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
+-> Tool Windows -> Maven Projects), find the generate task node
+(Plugins -> teamcity-configs -> teamcity-configs:generate), the
+'Debug' option is available in the context menu for the task.
+*/
+
+version = "2019.1"
+
+project {
+
+    features {
+        feature {
+            id = "PROJECT_EXT_2"
+            type = "OAuthProvider"
+            param("clientId", "1486291688@qq.com")
+            param("defaultTokenScope", "public_repo,repo,repo:status,write:repo_hook")
+            param("secure:clientSecret", "credentialsJSON:26992260-5acb-4eec-81f6-6972c7c15d3e")
+            param("displayName", "GitHub.com")
+            param("gitHubUrl", "https://github.com/")
+            param("providerType", "GitHub")
+        }
+    }
+}
